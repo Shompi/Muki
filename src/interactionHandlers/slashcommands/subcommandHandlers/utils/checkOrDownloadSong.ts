821e3a0976a74ae803066e594fbe7e081d11d2ae
ShompiFlen <shompire02@gmail.com>
2023-02-08 10:10:29 -0300

    asd

--- src/interactionHandlers/slashcommands/subcommandHandlers/utils/checkOrDownloadSong.ts
+++ src/interactionHandlers/slashcommands/subcommandHandlers/utils/checkOrDownloadSong.ts
@@ -40,7 +40,7 @@ async function Download(videoId: string) {
 		// "--audio-format opus",
 		//"--audio-quality 192K",
 		// eslint-disable-next-line no-useless-escape
-		'-o "downloads/%\(title\)s %\(id\)s.%\(ext\)s"',
+		"-o 'downloads/%\(title\)s %\(id\)s.%\(ext\)s'",
 		youtubeBaseUrl + videoId,
 		//"--simulate", // Do not download any video
 	]
@@ -49,7 +49,7 @@ async function Download(videoId: string) {
 
 		console.log("Creando child youtube-dl")
 		// eslint-disable-next-line no-useless-escape
-		const process = spawn("youtube-dl", ["-w -x -o 'downloads/%\(title\)s %\(id\)s.%\(ext\)s'"], { stdio: "inherit" })
+		const process = spawn("youtube-dl", ytdlArgs, { stdio: "inherit" })
 
 		process.on('spawn', () => {
 			console.log("Comenzando la descarga del video");
