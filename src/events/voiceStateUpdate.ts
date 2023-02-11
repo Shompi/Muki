c3af66cb278a5f3579399272fc525db8b6a4f0f0
Shompi <shompire02@gmail.com>
2023-02-11 05:53:01 -0300

    Disconnect Muki from the channel if there is no one else but her

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -1,3 +1,4 @@
+import { AudioPlayerStatus } from "@discordjs/voice";
 import { EventFile } from "@myTypes/*";
 import { Events, VoiceState } from "discord.js";
 
@@ -6,8 +7,21 @@ export default {
 	once: false,
 	execute(oldState: VoiceState, newState: VoiceState) {
 
+		const { client: muki, channel, guild } = newState
 
+		// if there is no audiplayer created that means we are not playing any audio here.
+		if (!guild.audioPlayer) return
 
+		// if Muki is alone in the channel
+		if (channel?.members.size === 1 && channel?.members.has(muki.user.id)) {
+
+			// Check if Muki is playing audio
+			if (guild.audioPlayer.state.status === AudioPlayerStatus.Playing) {
+				// We have to disconnect her
+				
+
+			}
+		}
 		return
 	}
 } as EventFile
