62b130aecb6924c009e615e16d05fd4124167c0b
Shompi <shompire02@gmail.com>
2023-02-13 21:22:25 -0300

    Disconnect Muki from the voice channel if she is alone

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -1,6 +1,6 @@
-import { AudioPlayerStatus } from "@discordjs/voice";
+import { AudioPlayerStatus, getVoiceConnection } from "@discordjs/voice";
 import { EventFile } from "@myTypes/*";
-import { Events, VoiceState } from "discord.js";
+import { Events, TextChannel, VoiceState } from "discord.js";
 
 export default {
 	name: Events.VoiceStateUpdate,
@@ -18,8 +18,16 @@ export default {
 			// Check if Muki is playing audio
 			if (guild.audioPlayer.state.status === AudioPlayerStatus.Playing) {
 				// We have to disconnect her
-				
 
+				const connection = getVoiceConnection(guild.id)
+
+				if (connection && guild.queue) {
+					connection.disconnect()
+					const interactionChannel = guild.channels.cache.get(guild.queue?.channelId) as TextChannel
+					const SadEmoji = muki.emojis.cache.filter(emoji => emoji.name?.toLowerCase().includes('sad')).random()
+					// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+					void interactionChannel.send({ content: `<@${newState.member!.id}> me dejaste sola en el canal... ${SadEmoji ?? ""}` })
+				}
 			}
 		}
 		return
