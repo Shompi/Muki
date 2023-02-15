d643fcea603473fe933843f8ef7f21f986848a2b
ShompiFlen <shompire02@gmail.com>
2023-02-15 17:58:52 -0300

    Fixed bug preventing muki from disconnecting

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -7,24 +7,32 @@ export default {
 	once: false,
 	execute(oldState: VoiceState, newState: VoiceState) {
 
-		const { client: muki, channel, guild } = newState
+		const { client, guild } = newState
+
+		const connection = getVoiceConnection(guild.id)
+		if (!connection) return;
 
 		// if there is no audiplayer created that means we are not playing any audio here.
 		if (!guild.audioPlayer) return
+		console.log('The guild does have an audio player');
+
 
 		// if Muki is alone in the channel
-		if (channel?.members.size === 1 && channel?.members.has(muki.user.id)) {
+		if (guild.members.me?.voice.channel?.members.size === 1) {
+			console.log("There is only 1 member on the voice channel");
 
 			// Check if Muki is playing audio
 			if (guild.audioPlayer.state.status === AudioPlayerStatus.Playing) {
+
 				// We have to disconnect her
+				console.log("Muki is playing audio");
 
 				const connection = getVoiceConnection(guild.id)
 
-				if (connection && guild.queue) {
+				if (connection) {
 					connection.disconnect()
-					const interactionChannel = guild.channels.cache.get(guild.queue?.channelId) as TextChannel
-					const SadEmoji = muki.emojis.cache.filter(emoji => emoji.name?.toLowerCase().includes('sad')).random()
+					const interactionChannel = guild.channels.cache.get(guild.queue!.channelId) as TextChannel
+					const SadEmoji = client.emojis.cache.filter(emoji => emoji.name?.toLowerCase().includes('sad')).random()
 					// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
 					void interactionChannel.send({ content: `<@${newState.member!.id}> me dejaste sola en el canal... ${SadEmoji ?? ""}` })
 				}
