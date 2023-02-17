04fb18e55ee20ff01da4924d62bff0e2a29902e3
ShompiFlen <shompire02@gmail.com>
2023-02-17 00:52:39 -0300

    Added welcome image to welcome new members

---
+++ src/events/guildMemberAdd.ts
@@ -0,0 +1,23 @@
+import { EventFile } from "@myTypes/*";
+import { Events, GuildMember } from "discord.js";
+import { GenerateWelcomeImage } from "./utils/generate";
+
+
+
+const event: EventFile = {
+	name: Events.GuildMemberAdd,
+	once: false,
+	async execute(member: GuildMember) {
+		const defaultChannel = member.guild.systemChannel
+
+		if (!defaultChannel) return
+
+		return void defaultChannel.send({
+			// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+			content: `¡Bienvenido al servidor ${member}!`,
+			files: [await GenerateWelcomeImage(member)]
+		})
+	}
+}
+
+export default event
