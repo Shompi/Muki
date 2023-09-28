5a1272f28da7da941998ae036550603a5b6a5522
ShompiFlen <shompire02@gmail.com>
2023-09-28 20:45:46 -0300

    Filtered out channel types that don't have permissionsOverwrites

--- src/interactionHandlers/buttons/channel-reopen.ts
+++ src/interactionHandlers/buttons/channel-reopen.ts
@@ -1,28 +1,27 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonInteraction } from "npm:discord.js@14.13.0";
+import { ActionRowBuilder, ButtonBuilder, ButtonInteraction } from "npm:discord.js";
 
 export async function ReopenChannel(interaction: ButtonInteraction<'cached'>) {
 
-	const { member, channel, guildId } = interaction
+	const { member, channel } = interaction
 
 	if (member.permissions.any(["ModerateMembers", "ManageChannels"])) {
 		try {
-			await channel!.edit({
-				permissionOverwrites: [
-					{
-						id: guildId,
-						allow: ["SendMessages"]
-					}
-				]
-			})
 
-			const button = ButtonBuilder.from(interaction.component).setDisabled(true)
-			const row = new ActionRowBuilder<ButtonBuilder>().setComponents(button)
+			if (channel && !channel.isThread() && !channel.isDMBased()) {
 
-			await interaction.update({
-				content: 'El canal ha sido desbloqueado.',
-				components: [row]
-			})
+				await interaction.deferReply()
+				await channel.permissionOverwrites.edit(channel.guild.roles.everyone, {
+					SendMessages: null
+				})
 
+				const button = ButtonBuilder.from(interaction.component).setDisabled(true)
+				const row = new ActionRowBuilder<ButtonBuilder>().setComponents(button)
+
+				await interaction.editReply({
+					content: 'El canal ha sido desbloqueado.',
+					components: [row]
+				})
+			}
 		}
 		catch (e) {
 			console.log(e);
